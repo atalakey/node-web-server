1c02946129ac8985cf4fd1b4c0e53688f5bc1e62
Abdullah Talakey <abdullah.talakey@gmail.com>
2018-08-24 12:57:31 +0300

    render template with dynamic data using hbs as Express view engine

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerPartials(__dirname + '/views/partials');
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>');
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About Page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
 });
 
 app.get('/bad', (req, res) => {
